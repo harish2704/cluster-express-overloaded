3ec0c88b906341f993663d38cb88f273257c8f75
Varun Batra <codevarun@gmail.com>
2017-05-04 14:24:04 +0530

    Added empty directory of components

--- boot/index.js
+++ boot/index.js
@@ -3,7 +3,6 @@ var onlyjs = /.js$/
 var boot = {}
 
 boot.config = requireDirectory(module, './../config');
-boot.components = requireDirectory(module, './../components', {include: onlyjs});
 boot.override = requireDirectory(module, './../override', {include: onlyjs});
 boot.components = requireDirectory(module, './../components', {include: onlyjs})
 module.exports = boot
