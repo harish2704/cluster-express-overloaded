f3bbb546c6280d492f566a11c6c48e99995e32bb
Varun Batra <codevarun@gmail.com>
2017-05-06 13:42:58 +0530

    removed log

--- boot/index.js
+++ boot/index.js
@@ -6,5 +6,4 @@ boot.config = requireDirectory(module, './../config');
 boot.override = requireDirectory(module, './../override', {include: onlyjs});
 boot.components = requireDirectory(module, './../components', {include: onlyjs})
 boot.routes = requireDirectory(module, './../routes', {include: onlyjs})
-console.log(boot);
 module.exports = boot
