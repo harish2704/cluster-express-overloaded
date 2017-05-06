ed7c3603670e3d0380dd3f99179e6a07ff94c3f6
Varun Batra <codevarun@gmail.com>
2017-05-06 19:19:23 +0530

    added favicon quick

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ var bunyan = require('bunyan')
 var log = bunyan.createLogger({name: app.name})
 log.level(app.config.app.logLevel)
 app.log = log
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   req.log = log.child({reqId: uuid()})
   next()
 })
@@ -50,7 +50,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
