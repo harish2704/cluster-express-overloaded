c3d819f135fa7e329f00ea3262923e5e16be4027
Varun Batra <codevarun@gmail.com>
2017-05-08 11:35:56 +0530

    redis only if config exist

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -2,7 +2,7 @@ let app = global.app
 var session = require('express-session')
 var RedisStore = require('connect-redis')(session)
 // redis session
-if (app.config.app.redisSession) {
+if (typeof app.config.app.redisSession !== "undefined") {
   app.use(session({
     store: new RedisStore(app.config.app.redisSession),
     secret: app.config.app.secret
